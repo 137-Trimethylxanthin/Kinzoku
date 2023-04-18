97e941717ec5297659cf6fc406be79f69f6733f9
SitterM <xxv.many@gmail.com>
2023-04-18 14:44:40 +0200

    test

---
+++ kinzoku_maxi.py
@@ -0,0 +1,19 @@
+import random
+import sys
+import time
+"""
+a math game.
+you need to calculate tow random number.
++ - * /
+
+you get point based on your speed
+unter 10 sec = 3 point
+unter 20 sec = 2 point
+ab 20 sec = 1 point
+
+false answer = 0 point
+
+you get 10 problems to solver
+"""
+
+
