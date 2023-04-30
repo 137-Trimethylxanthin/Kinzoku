ecf784551897bed3e3cca41d414c5ac4f1947c44
maxi <xxv.many@gmail.com>
2023-04-30 13:51:35 +0200

    redid some things

--- kinzoku_maxi.py
+++ kinzoku_maxi.py
@@ -1,11 +1,7 @@
-import datetime
+#!/usr/bin/env python3
+import os
 import random
 import time
-import os
-import sys
-
-if sys.argv[1] == 'gui':
-    from tkinter import *
 
 
 def clear():
@@ -15,9 +11,9 @@ def clear():
 """
 a game the language is German
 
-the game is a math game where you have to awnser 10 questions
+the game is a math game where you have to answer 10 questions
 the questions are about multiplication and division and addition and subtraction
-the questions are random in the thousend range
+the questions are random in the thousand range
 under 10 seconds you get 3 points
 under 20 seconds you get 2 points
 over 20 seconds you get 1 point
@@ -38,20 +34,69 @@ def game_start():
     input()
 
 
+def get_division():
+    num1: int = random.randint(1, 1000)
+    num2: int = random.randint(1, 1000)
+    while num1 % num2 != 0 or num1 < num2:
+        num1 = random.randint(1, 1000)
+        num2 = random.randint(1, 1000)
+    return num1, num2
+
+
+def get_multiplication():
+    num1: int = random.randint(1, 1000)
+    num2: int = random.randint(1, 1000)
+
+    while num1 * num2 > 2000:
+        num1 = random.randint(1, 1000)
+        num2 = random.randint(1, 1000)
+
+    return num1, num2
+
+
+def get_subtraction():
+    num1: int = random.randint(1, 1000)
+    num2: int = random.randint(1, 1000)
+
+    while num1 < num2:
+        num1 = random.randint(1, 1000)
+        num2 = random.randint(1, 1000)
+
+    return num1, num2
+
+
+def get_addition():
+    num1: int = random.randint(1, 1000)
+    num2: int = random.randint(1, 1000)
+
+    while num1 + num2 > 2000:
+        num1 = random.randint(1, 1000)
+        num2 = random.randint(1, 1000)
+
+    return num1, num2
+
+
 def get_random_question():
-    num1 = random.randint(1, 1000)
-    num2 = random.randint(1, 1000)
-    operator = random.choice(['+', '-', '*', '/'])
-    if operator == '/':
-        while num1 % num2 != 0:
-            num1 = random.randint(1, 1000)
-            num2 = random.randint(1, 1000)
+    operator: str = random.choice(['+', '-', '*', '/'])
+    num1: int = 0
+    num2: int = 0
+    match operator:
+        case '+':
+            num1, num2 = get_addition()
+        case '-':
+            num1, num2 = get_subtraction()
+        case '*':
+            num1, num2 = get_multiplication()
+        case '/':
+            num1, num2 = get_division()
+
     return num1, num2, operator
 
 
-def custom_input():
+def custom_input(question: str):
     while True:
         try:
+            print(question, end="")
             answer = int(input())
         except ValueError:
             print("Bitte eine Zahl eingeben")
@@ -60,100 +105,59 @@ def custom_input():
             return answer
 
 
-def answer(num1, num2, op, answer: int):
+def test_answer(num1, num2, op, answer: int):
     if int(eval(f"{num1} {op} {num2}")) == int(answer):
         return True
     else:
         return False
 
 
-def gui():
-    root = Tk()
-    root.title("Mathe Spiel")
-    lab = Label(root)
-    lab.pack()
-    root.geometry("500x800")
-    root.resizable(False, False)
-    root.configure(bg="black")
+def end_game(punkte: int):
+    term_x: int = os.get_terminal_size().columns
+    term_y: int = os.get_terminal_size().lines
 
-    clac_flield = Entry(root, width=500, borderwidth=5)
-    clac_flield.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-    clac_flield.pack()
+    clear()
+    print("Das Spiel ist zu Ende".center(term_x, "-"))
+    for i in range(term_y // 2 - 2):
+        print()
 
-    # Buttons for calculator
-    one = Button(root, text="1", width=10, height=5, bg="white", fg="black")
-    one.grid(row=0, column=1)
+    if punkte == 1:
+        print(f"Du hast {punkte} Punkt".center(term_x))
+    else:
+        print(f"Du hast {punkte} Punkte erreicht".center(term_x))
 
-    two = Button(root, text="2", width=10, height=5, bg="white", fg="black")
-    two.grid(row=1, column=1)
+    if punkte == 30:
+        print("Du hast alle Fragen unter 10 sekunden beantwortet".center(term_x))
+    elif punkte >= 20:
+        print("Du hast die meisten Fragen richtig beantwortet".center(term_x))
+    elif punkte >= 10:
+        print("Du hast die Hälfte der Fragen richtig beantwortet".center(term_x))
+    elif punkte == 0:
+        print("Du hast alle Fragen falsch beantwortet".center(term_x))
+    else:
+        print("Du hast die meisten Fragen falsch beantwortet".center(term_x))
 
-    three = Button(root, text="3", width=10, height=5, bg="white", fg="black")
-    three.grid(row=2, column=1)
-
-    four = Button(root, text="4", width=10, height=5, bg="white", fg="black")
-    four.grid(row=0, column=2)
-
-    five = Button(root, text="5", width=10, height=5, bg="white", fg="black")
-    five.grid(row=1, column=2)
-
-    six = Button(root, text="6", width=10, height=5, bg="white", fg="black")
-    six.grid(row=2, column=2)
-
-    seven = Button(root, text="7", width=10, height=5, bg="white", fg="black")
-    seven.grid(row=0, column=3)
-
-    eight = Button(root, text="8", width=10, height=5, bg="white", fg="black")
-    eight.grid(row=1, column=3)
-
-    nine = Button(root, text="9", width=10, height=5, bg="white", fg="black")
-    nine.grid(row=2, column=3)
-
-    zero = Button(root, text="0", width=10, height=5, bg="white", fg="black")
-    zero.grid(row=3, column=0)
-
-    plus = Button(root, text="+", width=10, height=5, bg="white", fg="black")
-
-    minus = Button(root, text="-", width=10, height=5, bg="white", fg="black")
-
-    mal = Button(root, text="*", width=10, height=5, bg="white", fg="black")
-
-    durch = Button(root, text="/", width=10, height=5, bg="white", fg="black")
-
-    clear = Button(root, text="C", width=10, height=5, bg="white", fg="black")
-
-    enter = Button(root, text="Enter", width=10, height=5, bg="white", fg="black")
+    for i in range(term_y // 2 - 2):
+        print()
 
 
-    for r in range(3):
-        for c in range(4):
-            Label(root, text='R%s/C%s' % (r, c), borderwidth=1).grid(row=r, column=c)
-    def clock():
-        time = datetime.datetime.now().strftime("Time: %H:%M:%S")
-        lab.config(text=time)
-        root.after(1000, clock)  # run itself again after 1000 ms
-
-    clock()
-    root.mainloop()
-
-
-def game():
+def game_cli():
+    game_start()
     questions: int = 10
     points: int = 0
-    timer: float = 0
     clear()
     while questions > 0:
         num1, num2, op = get_random_question()
-        print(f"{num1} {op} {num2} = ", end="")
         start = time.time()
-        answer = custom_input()
+        answer: int = custom_input(f"{num1} {op} {num2} = ")
         end = time.time()
         questions -= 1
-        timer = end - start
-        if answer(num1, num2, op, answer):
+        timer: float = end - start
+        if test_answer(num1, num2, op, answer):
             print("Richtig")
-            if timer < 10:
+            if timer <= 10:
                 points += 3
-            elif timer < 20:
+            elif timer <= 20:
                 points += 2
             else:
                 points += 1
@@ -164,20 +168,15 @@ def game():
         print(f"Du hast {timer.__round__(2)} Sekunden gebraucht")
         if questions == 1:
             print(f"Du hast noch {questions} Frage, lets go")
-            print(f"-------------------nexte Frage-------------------")
+            print(f"-------------------Nächste Frage-------------------")
         elif questions == 0:
             pass
         else:
             print(f"Du hast noch {questions} Fragen")
-            print(f"-------------------nexte Frage-------------------")
+            print(f"-------------------Nächste Frage-------------------")
 
-    print("--------------------Das Spiel ist vorbei-------------------")
-    print(f"Du hast {points} Punkte erreicht")
+    end_game(points)
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'gui':
-        gui()
-    else:
-        game_start()
-        game()
+    game_cli()
